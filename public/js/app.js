51a5a6169069623c5fd08613ca2495581919a372
faq84 <48797581+faq84@users.noreply.github.com>
2019-07-27 14:14:27 +0400

    styling css
    styling css and front end images

--- public/js/app.js
+++ public/js/app.js
@@ -17,19 +17,28 @@ console.log('client side javascript loaded!')
 
 const weatherForm= document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
 
 weatherForm.addEventListener('submit',(event)=>{
     event.preventDefault()
     const location = search.value
 
-
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
     fetch('http://localhost:8080/weather?city='+location, {'mode': 'no-cors'}).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
-                console.log('erro')
+                //console.log(data.error)
+                messageOne.textContent = data.error
             }else{
-                console.log(data.location)
-               console.log(data.forecastdata)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecastdata
+            //     console.log(data.location)
+            //    console.log(data.forecastdata)
             }
             
             
